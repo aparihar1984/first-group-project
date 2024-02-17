c987888758d9be237c9dfa73d1dbcbd5b07d9871
Kelsey Somers <kelseysomers@Kelseys-MacBook-Pro.local>
2024-02-17 17:05:02 -0500

    updated char.js by adding a display function

--- assets/javascript/char.js
+++ assets/javascript/char.js
@@ -17,7 +17,8 @@ var formSubmitHandler = function (event) {
 };
 
 var getUserRepos = function (user) {
-  var apiUrl = 'https://akabab.github.io/superhero-api/api/all.json/' + user + '/id';
+  // var apiUrl = 'https://akabab.github.io/superhero-api/api/all.json/' + user + '/id';
+  // var apiUrl = 'http://img.omdbapi.com/?apikey=[e1173bc0]' + user + '/t'
 
   fetch(apiUrl)
     .then(function (response) {
@@ -28,6 +29,32 @@ var getUserRepos = function (user) {
         });
       } 
     });
+};
+
+var displayRepos = function (repos, searchTerm) {
+  if (repos.length === 0) {
+    repoContainerEl.textContent = 'No characters found.';
+    return;
+  }
+
+  repoSearchTerm.textContent = searchTerm;
+
+  for (var i = 0; i < repos.length; i++) {
+    var repoName = repos[i].name;
+
+    var repoEl = document.createElement('a');
+    repoEl.setAttribute('href', './single-repo.html?id=' + repoName);
+
+    var titleEl = document.createElement('span');
+    titleEl.textContent = repoName;
+
+    repoEl.appendChild(titleEl);
+
+    var statusEl = document.createElement('span');
+    statusEl.classList = 'flex-row align-center';
+
+    repoContainerEl.appendChild(repoEl);
+  }
 };
 
 userFormEl.addEventListener('submit', formSubmitHandler);
