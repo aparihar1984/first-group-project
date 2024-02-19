604a07fe94c408834ce630e718d0b464a8601fcc
Kelsey Somers <kelseysomers@Kelseys-MacBook-Pro.local>
2024-02-18 19:31:49 -0500

    completing char.js and correcting char.html

--- assets/javascript/char.js
+++ assets/javascript/char.js
@@ -32,11 +32,27 @@ try {
 } catch (error) {
   console.error(error);
 }
-
-
-
 };
 
+var displayCard = function(result) {
+  var superheroesList = document.getElementById('superheroes-list')
+  superheroesList.textContent = '';
+    result.hero.forEach(superhero => {
+      const superheroCard = document.createElement('div');
+      superheroCard.classList.add('superhero-card');
+      const superheroName = document.createElement('h2');
+      superheroName.textContent = superhero.data.name;
+      const superheroImage = document.createElement('img');
+      superheroImage.src = superhero.data.image.url;
+      superheroImage.alt = superhero.data.name;
+      const superheroPowerstats = document.createElement('p');
+      superheroPowerstats.textContent = `Intelligence: ${superhero.data.powerstats.intelligence}, Strength: ${superhero.data.powerstats.strength}, Speed: ${superhero.data.powerstats.speed}`;
+      superheroCard.appendChild(superheroName);
+      superheroCard.appendChild(superheroImage);
+      superheroCard.appendChild(superheroPowerstats);
+      superheroesList.appendChild(superheroCard);
+  });
+  }
 
 userFormEl.addEventListener('submit', formSubmitHandler);
 
